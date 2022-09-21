843d3bf11a8f89a9ce3c4881a6e22577db6d7820
Amit Parajuli <parajuliamit@gmail.com>
2022-09-21 16:32:10 +0545

    chat completed

--- lib/app/modules/messages/chat/controllers/chat_controller.dart
+++ lib/app/modules/messages/chat/controllers/chat_controller.dart
@@ -1,10 +1,22 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:owner_app/app/app_controller.dart';
 import 'package:owner_app/app/app_repository.dart';
 import 'package:owner_app/app/data/exception/server_exception.dart';
 import 'package:owner_app/app/data/models/chat/message_request.dart';
 import 'package:owner_app/app/utils/app_utils.dart';
+
+import '../../../../data/models/chat/message_response.dart';
+
+class FormattedMessage {
+  String message;
+  bool isMe;
+
+  FormattedMessage(this.message, this.isMe);
+}
 
 class ChatController extends GetxController {
   late final String tenantName;
@@ -13,7 +25,15 @@ class ChatController extends GetxController {
   final chatRepo = Get.find<AppRepository>().getChatRepository();
 
   final isLoading = false.obs;
+  final isMessageLoading = false.obs;
+  final isError = false.obs;
+  String errorMessage = '';
+  late Timer timer;
+
   late final TextEditingController messageController;
+
+  List<Message> messages = [];
+  List<FormattedMessage> formattedMessage = [];
 
   @override
   void onInit() {
@@ -21,6 +41,10 @@ class ChatController extends GetxController {
     tenantName = Get.parameters['name']!;
     tenantId = Get.parameters['tenantId']!;
     messageController = TextEditingController();
+    loadMessage();
+    timer = Timer.periodic(const Duration(seconds: 10), (_) {
+      loadMessage();
+    });
   }
 
   Future<void> sendMessage() async {
@@ -31,6 +55,8 @@ class ChatController extends GetxController {
     try {
       await chatRepo.sendMessage(
           MessageRequest(receiver: int.parse(tenantId), message: message));
+      formattedMessage.insert(0, FormattedMessage(message, true));
+      update();
       messageController.clear();
     } catch (e) {
       if (e is DioError) {
@@ -44,6 +70,30 @@ class ChatController extends GetxController {
     isLoading(false);
   }
 
+  Future<void> loadMessage() async {
+    if (formattedMessage.isEmpty) isMessageLoading(true);
+    isError(false);
+
+    try {
+      messages = await chatRepo.getMessages(tenantId);
+      for (var message in messages) {
+        formattedMessage.add(FormattedMessage(message.message ?? '',
+            Get.find<AppController>().profile?.id == message.sender));
+      }
+      formattedMessage = formattedMessage.reversed.toList();
+      if (formattedMessage.isNotEmpty) update();
+    } catch (e) {
+      isError(true);
+      if (e is DioError) {
+        errorMessage = ServerError.withError(error: e).getErrorMessage();
+      } else {
+        errorMessage = e.toString();
+      }
+    }
+
+    isMessageLoading(false);
+  }
+
   @override
   void onReady() {
     super.onReady();
@@ -52,5 +102,6 @@ class ChatController extends GetxController {
   @override
   void onClose() {
     messageController.dispose();
+    timer.cancel();
   }
 }
--- lib/app/modules/messages/chat/views/chat_view.dart
+++ lib/app/modules/messages/chat/views/chat_view.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:owner_app/app/utils/constants.dart';
+import 'package:owner_app/app/widgets/error_page.dart';
 
+import '../../../../widgets/loading.dart';
 import '../controllers/chat_controller.dart';
 import 'widgets/chat_bubble.dart';
 
@@ -13,75 +15,86 @@ class ChatView extends GetView<ChatController> {
         appBar: AppBar(
           title: Text(controller.tenantName),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: 10,
-                  reverse: true,
-                  itemBuilder: (context, index) {
-                    return ChatBubble(
-                      isMe: index % 2 == 0,
-                    );
-                  }),
-            ),
-            const Divider(
-              thickness: 1,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 5, 5, 10),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: controller.messageController,
-                      cursorColor: kPrimaryColor,
-                      style: const TextStyle(fontSize: 17),
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 12, horizontal: 15),
-                          filled: true,
-                          isDense: true,
-                          fillColor: kPrimaryColor.withOpacity(0.1),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                width: 2,
-                                color: kPrimaryColor,
-                              ),
-                              borderRadius: BorderRadius.circular(15)),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: kPrimaryColor.withOpacity(0.7),
-                              ),
-                              borderRadius: BorderRadius.circular(15)),
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: kPrimaryColor.withOpacity(0.7),
-                              ),
-                              borderRadius: BorderRadius.circular(15))),
-                    ),
-                  ),
-                  Obx(() => controller.isLoading.isTrue
-                      ? Container(
-                          padding: const EdgeInsets.all(5),
-                          width: 30,
-                          height: 30,
-                          child: const CircularProgressIndicator(
-                            color: kPrimaryColor,
-                          ),
-                        )
-                      : IconButton(
-                          onPressed: () {
-                            controller.sendMessage();
+        body: Obx(() => controller.isMessageLoading.isTrue
+            ? const Loading()
+            : controller.isError.isTrue
+                ? ErrorPage(controller.errorMessage, controller.loadMessage)
+                : Column(
+                    children: [
+                      Expanded(
+                        child: GetBuilder<ChatController>(
+                          builder: (controller) {
+                            return ListView.builder(
+                                itemCount: controller.formattedMessage.length,
+                                reverse: true,
+                                itemBuilder: (context, index) {
+                                  return ChatBubble(
+                                    message: controller.formattedMessage[index],
+                                  );
+                                });
                           },
-                          icon: const Icon(
-                            Icons.send,
-                            color: kPrimaryColor,
-                          )))
-                ],
-              ),
-            )
-          ],
-        ));
+                        ),
+                      ),
+                      const Divider(
+                        thickness: 1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 5, 5, 10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: controller.messageController,
+                                cursorColor: kPrimaryColor,
+                                style: const TextStyle(fontSize: 17),
+                                decoration: InputDecoration(
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        vertical: 12, horizontal: 15),
+                                    filled: true,
+                                    isDense: true,
+                                    fillColor: kPrimaryColor.withOpacity(0.1),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                          width: 2,
+                                          color: kPrimaryColor,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: kPrimaryColor.withOpacity(0.7),
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: kPrimaryColor.withOpacity(0.7),
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(15))),
+                              ),
+                            ),
+                            Obx(() => controller.isLoading.isTrue
+                                ? Container(
+                                    padding: const EdgeInsets.all(5),
+                                    width: 30,
+                                    height: 30,
+                                    child: const CircularProgressIndicator(
+                                      color: kPrimaryColor,
+                                    ),
+                                  )
+                                : IconButton(
+                                    onPressed: () {
+                                      controller.sendMessage();
+                                    },
+                                    icon: const Icon(
+                                      Icons.send,
+                                      color: kPrimaryColor,
+                                    )))
+                          ],
+                        ),
+                      )
+                    ],
+                  )));
   }
 }
